729efaf176246d73d254a29741252175dd02c9f3
quent <qmeeus@gmail.com>
2018-10-30 18:16:36 +0100

    make model and data classes

--- model/keyword_detection.py
+++ model/keyword_detection.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import time
@@ -16,82 +17,93 @@ URL_RGX = r"((http|ftp|https):\/\/)?([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+
 
 
 class Config:
-    filename = "data/english.csv"
-    model_file = "outputs/model.pkl"
+    filename = os.path.dirname(os.path.abspath(__file__)) + "/data/english.csv"
+    model_file = os.path.dirname(os.path.abspath(__file__)) + "/outputs/model.pkl"
     nrows = 100
 
     count_vect = {}
     tfidf_vect = {"stop_words": "english"}
 
 
+class Model:
+
+    def __init__(self, config, load_from_file=False):
+        self.config = config
+        self.pipeline = None
+        if load_from_file:
+            self.pipeline = self.load()
+            self.is_trained = True
+        else:
+            self.build_model()
+            self.is_trained = False
+
+    def build_model(self):
+        # vectorizer = CountVectorizer(config.count_vect)
+        vectorizer = TfidfVectorizer(**self.config.tfidf_vect)
+        pipeline = Pipeline([("vect", vectorizer)])
+        self.pipeline = pipeline
+
+    def train(self, data):
+        t0 = time.time()
+        self.pipeline.fit(data)
+        logger.info("Model trained in {:.2f} s.".format(time.time() - t0))
+        self.is_trained = True
+
+    def predict(self, data, top=5):
+        model = self.pipeline
+        features = model.named_steps["vect"].get_feature_names()
+        predictions = model.transform(data).tocoo()
+        dense = list(zip(predictions.row, predictions.col, predictions.data))
+        return (pd.DataFrame.from_records(dense, columns=["row", "column", "tfidf"])
+                .sort_values(["row", "tfidf"], ascending=[True, False])
+                .assign(features=lambda df: df["column"].map(lambda i: features[i]))
+                .drop("column", axis=1)
+                .set_index(["row", "features"])
+                .groupby(level=0)
+                .head(top))
+
+    def save(self):
+        with open(self.config.filename, "wb") as f:
+            pickle.dump(self.pipeline, f)
+
+    def load(self):
+        with open(self.config.model_file, "rb") as f:
+            return pickle.load(f)
+
+
+class Data:
+    def __init__(self, config):
+        t0 = time.time()
+
+        def clean(df):
+            df["tweet_time"] = pd.to_datetime(df["tweet_time"], format="%Y-%m-%d %H:%M")
+            df["tweet_text"] = df["tweet_text"].str.replace(URL_RGX, "%URL%")
+            return df.set_index('tweet_time')[["tweet_text"]]
+
+        data = pd.read_csv(config.filename, nrows=config.nrows)
+        data = clean(data)
+
+        logger.info("Loaded {} rows x {} cols in {:.2f} s,".format(*data.shape, time.time() - t0))
+        self.values = data
+
+    def preprocess(self):
+        data = self.values
+        # def _preprocessing(text):
+        #     return pos_tag(word_tokenize(text))
+
+        # data["pos_tags"] = data["tweet_text"].map(preprocessing)
+        self.values = data
+
+
 def main():
     config = Config()
-    data = load_data(config.filename, config.nrows)
-    data = preprocessing(data)
-    model = build_model(config)
-    train(model, data["tweet_text"])
-    save_model(model, config.model_file)
-    predictions = predict(model, data["tweet_text"].values[:10])
+    model = Model(config)
+    data = Data(config)
+    model.train(data.values["tweet_text"])
+    model.save()
+    predictions = model.predict(data.values["tweet_text"].values[:10])
     logger.info(predictions)
 
-
-def save_model(model, filename):
-    with open(filename, "wb") as f:
-        pickle.dump(model, f)
-
-
-def load_model(filename):
-    with open(filename, "rb") as f:
-        return pickle.load(f)
-
-
-def load_data(filename, nrows=None):
-    t0 = time.time()
-
-    def clean(df):
-        df["tweet_time"] = pd.to_datetime(df["tweet_time"], format="%Y-%m-%d %H:%M")
-        df["tweet_text"] = df["tweet_text"].str.replace(URL_RGX, "%URL%")
-        return df.set_index('tweet_time')[["tweet_text"]]
-
-    data = pd.read_csv(filename, nrows=nrows)
-    data = clean(data)
-
-    logger.info("Loaded {} rows x {} cols in {:.2f} s,".format(*data.shape, time.time() - t0))
-    return data
-
-
-def preprocessing(data):
-    # def _preprocessing(text):
-    #     return pos_tag(word_tokenize(text))
-
-    # data["pos_tags"] = data["tweet_text"].map(preprocessing)
-    return data
-
-def build_model(config):
-    # vectorizer = CountVectorizer(config.count_vect)
-    vectorizer = TfidfVectorizer(**config.tfidf_vect)
-    pipeline = Pipeline([("vect", vectorizer)])
-    return pipeline
-
-
-def train(model, data):
-    t0 = time.time()
-    model.fit(data)
-    logger.info("Model trained in {:.2f} s.".format(time.time() - t0))
-    return model
-
-
-def predict(model, data, top=5):
-    features = model.named_steps["vect"].get_feature_names()
-    predictions = model.transform(data).tocoo()
-    dense = list(zip(predictions.row, predictions.col, predictions.data))
-    return (pd.DataFrame.from_records(dense, columns=["row", "column", "tfidf"])
-            .sort_values(["row", "tfidf"], ascending=[True, False])
-            .assign(features=lambda df: df["column"].map(lambda i: features[i]))
-            .drop("column", axis=1)
-            .set_index(["row", "features"])
-            .groupby(level=0)
-            .head(top))
 
 # def download_nltk():
 #     import nltk
