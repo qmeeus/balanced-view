cc03187e60ede715b05b805e2db7e5bc3bf0e1de
quent <qmeeus@gmail.com>
2018-11-19 22:30:47 +0100

    api requests

--- app/api.py
+++ app/api.py
@@ -1,31 +1,42 @@
-# from model.keyword_detection import load_model, Config, predict
-# from model.keyword_detection import Config, Model
+# from summa import keywords
+# import requests
+import re
 from summa import keywords
-
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-
-bp = Blueprint('index', __name__, url_prefix='/')
-# model = Model(Config(), load_from_file=True)
+from newsapi import NewsApiClient
 
 
-@bp.route('/', methods=('GET', 'POST'))
-def index():
+def load_key():
+    with open("apikey") as f:
+        return f.read().strip()
 
-    if request.method == 'POST':
-        text = request.form['text']
-        error = None
+def load_sources(language="en"):
+    # return 'bbc-news,the-verge'
+    return "msnbc,cnn"
 
-        if not text:
-            error = 'No text found.'
+def fetch_articles(text, start_date=None, end_date=None, language=None):
 
-        if error is None:
-            # TODO: logic send text to model -> get back results -> search NewsAPI -> render best results
-            # predictions = model.predict([text], n)
-            predictions = keywords.keywords(text, words=5, split=True, scores=True)
-            return render_template('results.html', predictions=predictions)
+    newsapi = NewsApiClient(api_key=load_key())
+    kws = keywords.keywords(text, words=3).replace("\n", " ")
+    print(kws)
+    return newsapi.get_everything(
+        q=kws,
+        sources=load_sources(language=language),
+        # domains='bbc.co.uk,techcrunch.com',
+        from_param=start_date or None,
+        to=end_date or None,
+        language=language or None,
+        sort_by='relevancy',
+        # page=2
+    )
 
-        flash(error)
 
-    return render_template('index.html')
+if __name__ == "__main__":
+    # Test API
+    text = """Democrat Stacey Abrams acknowledges that Republican Brian Kemp will be certified as 
+    the next Georgia governor, but says she is not offering a speech of concession because that 
+    would suggest the election process was just: "The state failed its voters" - @MSNBC"""
+    articles = fetch_articles(text, "2018-11-01")
+    print(articles["status"] + "... " + str(articles["totalResults"]) + " results")
+    for article in articles["articles"]:
+        print(article["source"]["name"])
+        print(article["title"])
---
+++ app/index.py
@@ -0,0 +1,40 @@
+# from model.keyword_detection import load_model, Config, predict
+# from model.keyword_detection import Config, Model
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+import re
+from summa import keywords
+from .api import fetch_articles
+
+bp = Blueprint('index', __name__, url_prefix='/')
+DATE_FORMAT_CHECK = re.compile(r"\d{4}-\d{2}-\d{2}")
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+
+    if request.method == 'POST':
+        text = request.form['text']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        error = None
+
+        if not text:
+            error = 'No text found.'
+
+        for date in (start_date, end_date):
+            if DATE_FORMAT_CHECK.match(date):
+                error = "Wrong date format. Required format: YYYY-MM-DD"
+
+        if error is None:
+            # TODO: logic send text to model -> get back results -> search NewsAPI -> render best results
+            # predictions = model.predict([text], n)
+            predictions = keywords.keywords(text, words=5, split=True, scores=True)
+            results = fetch_articles(text, start_date, end_date)
+            return render_template('results.html', predictions=predictions, articles=results)
+
+        flash(error)
+
+    return render_template('index.html')
