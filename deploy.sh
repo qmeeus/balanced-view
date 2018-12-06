6453cc066d7652856f6c5d6d383c68580c107e7d
quent <qmeeus@gmail.com>
2018-12-06 15:05:18 +0100

    update paths

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 app="fact-checker"
-heroku="$HOME/.local/bin/heroku/bin/heroku"
 
-$heroku container:push web --app $app
-$heroku container:release web --app $app
-$heroku open --app $app
+heroku container:push web --app $app
+heroku container:release web --app $app
+heroku open --app $app
 
