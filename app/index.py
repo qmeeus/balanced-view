4a288f57bc01550fb51576dc84344d3e7bb955ad
quent <qmeeus@gmail.com>
2018-12-15 19:05:46 +0100

    display graph as modal on index page

--- app/index.py
+++ app/index.py
@@ -5,7 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, validators
 import json
 
-from .api import fetch_articles, get_graph
+from .api.fakenewsapi import FakeNewsAPI
 
 
 bp = Blueprint('index', __name__, url_prefix='/')
@@ -24,14 +24,8 @@ def fact_checker():
     form = FactForm()
     if form.validate_on_submit():
         text = form.text.data
-        articles = fetch_articles(text)
-        return render_template('index.html', form=form, search_results=articles)
+        fakenewsapi = FakeNewsAPI(text)
+        articles = fakenewsapi.get_results()
+        graph_data = fakenewsapi.get_graph()
+        return render_template('index.html', form=form, search_results=articles, data=graph_data)
     return render_template('index.html', form=form)
-
-@bp.route('/graph', methods=["GET"])
-def graph():
-    text = """The Mueller Witch Hunt is a total disgrace. They are looking at supposedly stolen Crooked 
-    Hillary Clinton Emails (even though they don’t want to look at the DNC Server), but have no interest 
-    in the Emails that Hillary DELETED & acid washed AFTER getting a Congressional Subpoena!"""
-    graph_data = get_graph(text)
-    return render_template('graph.html', data=graph_data)
