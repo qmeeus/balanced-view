05bee3ff7f60f458486dea3bf56364d627c8a26a
Quentin Meeus <qmeeus@gmail.com>
2018-12-20 21:25:47 +0100

    don't continue if a command failed

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
+# NB: don't forget to `heroku login` and `heroku container:login` if necessary
+
 app="fact-checker"
 
-heroku container:push web --app $app
-heroku container:release web --app $app
-heroku open --app $app
+heroku container:push web --app $app && \
+    heroku container:release web --app $app && \
+    heroku open --app $app
 
