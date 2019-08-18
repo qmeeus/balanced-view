20fb5e17aaddd7efbc08ad47d72f79a2a99a0d97
Quentin Meeus <qmeeus@gmail.com>
2019-08-18 13:02:51 +0200

    fix target in ibm so that translate does not depend on identify

--- tests/test_ibm_api.py
+++ tests/test_ibm_api.py
@@ -11,5 +11,5 @@ text = """Zaak-Epstein: Amerikaanse Justitie richt pijlen nu op Epsteins medewer
 De dood van Jeffrey Epstein, de van misbruik verdachte Amerikaanse miljardair die afgelopen weekend zelfmoord pleegde in zijn cel, betekent niet het einde van het onderzoek. Justitie belooft nu haar pijlen te richten op mogelijke medeplichtingen. Ook is een grondig onderzoek bevolen naar de zelfdoding van Epstein. Minister van Justitie William Barr zegt dat er "ernstige onregelmatigheden" zijn gebeurd in de gevangenis."""
 target = translator.identify(text, return_all=True)
 print(target)
-translated = translator.translate(text, source="nl", target=target, return_all=False)
+translated = translator.translate(text, source="nl", target="en", return_all=False)
 print(translated)
