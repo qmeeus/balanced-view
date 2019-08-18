e3553df687c106440e347664294afe39d0130112
Quentin Meeus <qmeeus@gmail.com>
2019-08-18 12:58:32 +0200

    add google translate api

---
+++ app/api/clients/googletranslate_api.py
@@ -0,0 +1,17 @@
+import os, os.path as p
+from google.cloud.translate import Client
+
+
+#! If this is gonna work, need to add billing method to service. This can be done here:
+#! https://console.developers.google.com/apis/api/translate.googleapis.com/overview?project=142410202562
+
+
+credential_path = p.abspath(p.join(p.dirname(__file__), "resources/google-credentials.json"))
+if not p.exists(credential_path):
+    raise FileNotFoundError("Please, provide the private key for using Google Translate API")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
+
+
+class GoogleTranslator(Client):
+    pass
+
