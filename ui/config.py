a05da577ce06db1f5c2fbff14955a26f1d3b3e83
Quentin Meeus <qmeeus@gmail.com>
2019-08-21 13:13:40 +0200

    set host=0.0.0.0

--- ui/config.py
+++ ui/config.py
@@ -8,4 +8,4 @@ dotenv.load_dotenv(p.join(p.dirname(__file__), ".env"))
 class Base:
     DEBUG = True
     SECRET_KEY = os.environ["SECRET_KEY"]
-    SERVER_NAME = f"localhost:{os.environ['FLASK_RUN_PORT']}"
+    SERVER_NAME = f"0.0.0.0:{os.environ['FLASK_RUN_PORT']}"
