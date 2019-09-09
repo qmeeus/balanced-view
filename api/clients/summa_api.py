d3cef4b832034f91e9006c38d29b73fbaeb5a7f1
Quentin Meeus <qmeeus@gmail.com>
2019-09-09 18:06:26 +0200

    summa as sklearn API scheme

--- api/clients/summa_api.py
+++ api/clients/summa_api.py
@@ -1,28 +1,34 @@
 import pandas as pd
 from summa.keywords import keywords
 
-from ..utils.knapsack import knapsack_dp
+from api.utils.knapsack import knapsack_dp
 
 
 class Summary:
 
-
-    def __init__(self, text, language='english', stopwords=None):        
+    def __init__(self, language='english', stopwords=None):        
         self.language = language
         self.stopwords = stopwords
-        self.keywords, self.graph, self.lemma2words, self.scores = \
+
+    def fit_transform(self, text):
+        self.keywords_, self.graph_, self.lemma2words_, self.scores_ = \
             self._process_text(text)
+        return self.keywords_
+
+    def fit(self, text):
+        self.fit_transform(text)
+        return self
 
     def get_graph(self):
-        nodes, edges = self.graph.nodes(), []
-        for node_a, node_b in self.graph.edges():
+        nodes, edges = self.graph_.nodes(), []
+        for node_a, node_b in self.graph_.edges():
             if (node_b, node_a) not in edges and node_a != node_b:
                 edges.append((node_a, node_b))
 
         def node_info(node):
-            return {"name": self.lemma2words[node][0], 
+            return {"name": self.lemma2words_[node][0], 
                     "token": node, 
-                    "score": self.scores[node]}
+                    "score": self.scores_[node]}
 
         return {
             "nodes": list(map(node_info, nodes)),
@@ -33,7 +39,7 @@ class Summary:
 
     def get_keywords(self, max_kws=5):
         to_keep = self._keywords_to_keep(max_kws)
-        return " ".join(self.keywords['keyword'].iloc[to_keep].values)
+        return " ".join(self.keywords_['keyword'].iloc[to_keep].values)
 
     def _process_text(self, text):
         kwds, (graph, l2w, scores) = keywords(
@@ -49,8 +55,8 @@ class Summary:
         return kwds_df, graph, l2w, scores
 
     def _keywords_to_keep(self, max_kws):
-        weights = self.keywords['keyword'].map(lambda s: len(s.split())).values.tolist()
-        values = self.keywords['score'].values.tolist()
+        weights = self.keywords_['keyword'].map(lambda s: len(s.split())).values.tolist()
+        values = self.keywords_['score'].values.tolist()
         to_keep = knapsack_dp(values, weights, max_kws, max_kws)
         return to_keep
 
