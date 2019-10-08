c3f68800d3326e8059992cf281c5a06f02a78b84
Quentin Meeus <qmeeus@gmail.com>
2019-10-08 09:44:31 +0200

    remove test folder and move dev tests to notebooks (playground)

--- tests/integration_tests/__main__.py
+++
@@ -1,7 +0,0 @@
-from tests.integration_tests.test_balancedview import test_balancedview
-from tests.integration_tests.test_spider import test_source_collection
-
-
-test_balancedview()
-test_source_collection()
-
--- tests/system_tests/test_data_provider.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from api.data_provider import __main__ as main
-
-
-def test_data_provider():
-    main()
