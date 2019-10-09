9ed9e27ab5f6e6195ab5215a607841c9c43a3c32
Quentin Meeus <qmeeus@gmail.com>
2019-10-09 18:04:13 +0200

    setup authentication to es instance

--- api/data_provider/__init__.py
+++ api/data_provider/__init__.py
@@ -4,10 +4,10 @@ from typing import Optional, List
 
 from api.utils.patterns import Json
 
-
 hosts = [{"host": os.environ["ES_HOST"], "port": os.environ["ES_PORT"]}]
+auth = (os.environ["ES_USER", os.environ["ES_PWD"]])
 index_name = os.environ["ES_INDEX"]
-connections.create_connection(hosts=hosts)
+connections.create_connection(hosts=hosts, http_auth=auth)
 
 
 __all__ = [
