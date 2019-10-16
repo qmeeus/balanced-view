cfee35f8f2b222e62cbc270d7a64fd22fcd74500
Quentin Meeus <qmeeus@gmail.com>
2019-10-16 14:56:55 +0200

    add threading to simulate the server under pressure

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -3,13 +3,16 @@ import requests
 import json
 from operator import itemgetter
 import os
+from threading import Thread
+from queue import Queue
 
 from tests.test_articles import Meta
 from tests.utils import load_texts, load_keywords
 from api.utils.logger import logger
 
 
-API_LOCATION = f"http://localhost:{os.environ['FLASK_RUN_PORT']}"
+# API_LOCATION = f"http://localhost:{os.environ['FLASK_RUN_PORT']}"
+API_LOCATION = f"http://localhost:32597"
 
 
 class TestArticleEndpoint(unittest.TestCase, Meta):
@@ -29,33 +32,60 @@ class TestArticleEndpoint(unittest.TestCase, Meta):
 class TestAnalyseEndpoint(unittest.TestCase):
 
     ENDPOINT = API_LOCATION + "/analyse"
+    N_WORKERS = 12
+
+    DEFAULT_PARAMS = {
+        'related': True,
+        'search_languages': ['fr', 'en', 'nl'],
+        'groupby_options': {
+            "key": "language",
+            "default": "Other",
+            "groups": [
+                {"name": "French", "value": "fr"},
+                {"name": "Dutch", "value": "nl"},
+            ],
+            "orderby": "relevance",
+            "reverse": True,
+            "max_results_per_group": 3
+        }
+    }
+
+    def test_under_pressure(self):
+        queue = Queue()
+
+        def threader():
+            while True:
+                params = queue.get()
+                self._make_request(params)
+                queue.task_done()
+
+        for _ in range(self.N_WORKERS):
+            t = Thread(target=threader)
+            t.daemon = True
+            t.start()
+
+        params = self.DEFAULT_PARAMS.copy()
+        for _, text in load_texts():
+            params.update({'input_text': text})
+            queue.put(params)
+
+        queue.join()
 
     def test_endpoint(self):
+
+        params = self.DEFAULT_PARAMS.copy()
         for _, text in load_texts():
-            params = {
-                'input_text': text,
-                'related': True,
-                'search_languages': ['fr, en', 'nl'],
-                'groupby_options': {
-                    "key": "language",
-                    "default": "Other",
-                    "groups": [
-                        {"name": "French", "value": "fr"},
-                        {"name": "Dutch", "value": "nl"},
-                    ],
-                    "orderby": "relevance",
-                    "reverse": True,
-                    "max_results_per_group": 3
-                }
-            }
-    
-            response = self._make_request(params)
-            if "message" in response:
-                logger.debug(response)
-                return
-            self.assertIs(type(response), dict)
-            self.assertIn("articles", response)
-            self._test_groups(response["articles"], params["groupby_options"])
+            params.update({'input_text': text})
+            self._test_request(params)
+
+    def _test_request(self, params):
+        response = self._make_request(params)
+        if "message" in response:
+            logger.debug(response)
+            return
+        self.assertIs(type(response), dict)
+        self.assertIn("articles", response)
+        self._test_groups(response["articles"], params["groupby_options"])
 
     def _test_groups(self, groups, groupby_options):
         group_names = list(
