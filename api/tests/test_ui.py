df1aeca6507454306528625a445f60da9bba12f0
Quentin Meeus <qmeeus@gmail.com>
2019-10-16 15:24:00 +0200

    add ui under pressure to simulate many connections

--- api/tests/test_ui.py
+++ api/tests/test_ui.py
@@ -2,6 +2,8 @@ import requests
 import unittest
 import warnings
 import os
+from queue import Queue
+from threading import Thread
 
 from tests.utils import load_texts
 from api.utils.logger import logger
@@ -17,6 +19,8 @@ UI_LOCATION = "http://localhost:9999"
 
 class TestUI(unittest.TestCase):
 
+    N_WORKERS = 12
+
     def __init__(self, *args, **kwargs):
         r = requests.get(UI_LOCATION)
         if not r.status_code == 200:
@@ -25,11 +29,7 @@ class TestUI(unittest.TestCase):
 
     def test_ui(self):
         logger.debug(f"Start ui test @ {UI_LOCATION}")
-        resp = requests.get(UI_LOCATION)
-        resp.raise_for_status()
-
-        soup = BeautifulSoup(resp.text, "html.parser")
-        csrf_token = soup.find("input", {"id": "csrf_token"}).get("value")
+        csrf_token = self._get_csrf_token()
         assert bool(csrf_token)
 
         for i, (_, text) in enumerate(load_texts()):
@@ -37,6 +37,33 @@ class TestUI(unittest.TestCase):
             msg = f"Text #{i}: {title[:50]}... "
             n_results = self._test_post_request(text, csrf_token)
             logger.debug(msg + str(n_results) + " results")
+
+    def test_ui_under_pressure(self):
+        queue = Queue()
+        csrf_token = self._get_csrf_token()
+
+        def threader():
+            while True:
+                text = queue.get()
+                self._test_post_request(text, csrf_token)
+                queue.task_done()
+
+        for _ in range(self.N_WORKERS):
+            t = Thread(target=threader)
+            t.daemon = True
+            t.start()
+
+        for _, text in load_texts():
+            queue.put(text)
+
+        queue.join()
+
+    def _get_csrf_token(self):
+        resp = requests.get(UI_LOCATION)
+        resp.raise_for_status()
+
+        soup = BeautifulSoup(resp.text, "html.parser")
+        return soup.find("input", {"id": "csrf_token"}).get("value")
 
     def _test_post_request(self, text, csrf_token):
         params = {"text": text, "csrf_token": csrf_token}
